16457c44377636ddb900c4340aff0fa3e26f7fcd
Ilisav <vassili-ilouridze@hotmail.com>
2017-04-18 20:47:09 +0200

    map component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MapComponentComponent } from './map-component/map-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponentComponent
   ],
   imports: [
     BrowserModule,
